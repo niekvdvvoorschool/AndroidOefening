39596cd395b33ab5f6e2e202173a66a415b75ce7
Niek VDV <74327@rocteraa-student.nl>
2019-09-06 12:06:49 +0200

    Logcat button click

--- app/src/main/java/com/example/insertnamehere/MainActivity.kt
+++ app/src/main/java/com/example/insertnamehere/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.insertnamehere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bigButton.setOnClickListener {
+            d("Niek","button was pressed");
+        }
     }
 }

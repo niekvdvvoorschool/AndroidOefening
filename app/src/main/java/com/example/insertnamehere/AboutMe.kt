7fb3cd4d8665a4a8b2423d99b535528557f4610b
Niek VDV <74327@rocteraa-student.nl>
2019-09-06 13:23:24 +0200

    content

--- app/src/main/java/com/example/insertnamehere/AboutMe.kt
+++ app/src/main/java/com/example/insertnamehere/AboutMe.kt
@@ -1,13 +1,19 @@
 package com.example.insertnamehere
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_about_me.*
 
 class AboutMe : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_me)
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
     }
 
 }
